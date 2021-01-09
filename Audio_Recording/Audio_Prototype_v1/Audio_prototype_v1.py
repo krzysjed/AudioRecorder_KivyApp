c769b4fd9428bcd0f49eba9e8686c386860bbed0
Krzysztof Jędrejasz <krzysjed@wp.pl>
2021-01-09 09:45:01 +0000

    Replace Audio_prototype_v1.py

--- Audio_Recording/Audio_Prototype_v1/Audio_prototype_v1.py
+++ Audio_Recording/Audio_Prototype_v1/Audio_prototype_v1.py
@@ -1,16 +1,25 @@
+
+from kivy.core.window import Window
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from functools import partial
 import sounddevice as sd
 from scipy.io.wavfile import write
 from scipy.io import wavfile
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 import os
-kv = Builder.load_file("style.kv")
+from kivy.uix.switch import  Switch
+from kivy.uix.settings import SettingsWithTabbedPanel
+from settingsjson import settings_json
+from kivy.config import Config
+
+Builder.load_file("style.kv")
+
 
 class MainWindow(Screen):
+    def __init__(self, config_data):
+        super(MainWindow, self).__init__()
+        self.config_data = config_data
+
     def play(self):
         try:
             fs = 44000
@@ -24,9 +33,10 @@ class MainWindow(Screen):
 
         if self.rec_btn.text == "Record":
             self.rec_btn.text = "Recorded"
-            fs = 44000  # sampling
-            seconds = 5  # duration of recording
-            myrecording = sd.rec(seconds * fs, samplerate=fs, channels=2)
+            fs = 44000  # sampling max  384000  ;p
+            seconds = 10  # duration of recording
+            myrecording = sd.rec(seconds * fs, samplerate=fs, channels=int(self.config_data.get('example', 'channelsetting')))
+            print(self.config_data.get('example', 'channelsetting'))
             sd.wait()  # Wait until recording is finished
             write('output.wav', fs, myrecording)
             self.del_Btn.color = 1, 1, 1, 0.7
@@ -41,6 +51,10 @@ class MainWindow(Screen):
         self.del_Btn.color = 1, 1, 1, 0.2
         self.del_Btn.background_color = 0, 0, 0, 0.2
 
+    def exit(self):
+        App.get_running_app().stop()
+        Window.close()
+
 
 class AfterRecordWindow(Screen):
     pass
@@ -50,14 +64,48 @@ class WindowManager(ScreenManager):
     pass
 
 
-sm = ScreenManager()
-sm.add_widget(MainWindow(name="main"))
-sm.add_widget(AfterRecordWindow(name="second"))
+class SettingsScreen(Screen):
+    pass
 
 
 class MyPaintApp(App):
 
     def build(self):
+
+        self.settings_cls = SettingsWithTabbedPanel
+        self.use_kivy_settings = False
+
+        sm = ScreenManager()
+
+        #sm.add_widget(SettingsScreen(name='settings'))
+        #settings = self.config.get('example', 'channelsetting')
+
+        sm.add_widget(MainWindow(self.config))
         return sm
+
+    def build_config(self, config):
+        config.setdefaults('example', {
+            'ChannelSetting': '2',
+            'ExtensionSetting':'.wav',
+            'stringexample': 'some_string',
+            'pathexample': '/some/path'})
+
+    def build_settings(self, settings):
+
+        settings.add_json_panel('Audio',
+                                self.config,
+                                data=settings_json)
+
+        settings.add_json_panel('Cloud',
+                                self.config,
+                                data=settings_json)
+
+        settings.add_json_panel('Another itd',
+                                self.config,
+                                data=settings_json)
+
+    def on_config_change(self, config, section, key, value):
+        print(config, section, key, value)
+
 
 MyPaintApp().run()
