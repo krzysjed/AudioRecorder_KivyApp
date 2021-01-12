7bebd7cd6617129a9a893f88843524cc57578b34
Krzysztof Jędrejasz <krzysjed@wp.pl>
2021-01-12 15:35:33 +0000

    Replace settingsjson.py

--- Audio_Recording/Audio_Prototype_v1/settingsjson.py
+++ Audio_Recording/Audio_Prototype_v1/settingsjson.py
@@ -1,29 +1,64 @@
 import json
 
-settings_json = json.dumps([
+Recording_settings_json = json.dumps([
 
     {'type': 'options',
      'title': 'Numbers of Channels',
      'desc': 'Numbers of Channels 1-2',
-     'section': 'example',
+     'section': 'Record',
      'key': 'ChannelSetting',
      'options': ['1', '2']},
 
-     {'type': 'options',
-      'title': 'Extension',
-      'desc': 'Extension',
-      'section': 'example',
-      'key': 'ExtensionSetting',
-      'options': ['.wav', '.mp3','.flac']},
+    {'type': 'numeric',
+     'title': 'Duration',
+     'desc': 'Audio recording time',
+     'section': 'Record',
+     'key': 'DurationSetting'},
+
+    {'type': 'numeric',
+     'title': 'Sampling',
+     'desc': 'Sound recording frequency(1k-384k)',
+     'section': 'Record',
+     'key': 'SamplingSetting'},
+
+    {'type': 'options',
+     'title': 'Extension',
+     'desc': 'Extension',
+     'section': 'Record',
+     'key': 'ExtensionSetting',
+     'options': ['.wav', '.mp3','.flac']},
 
     {'type': 'string',
-     'title': 'A string setting',
-     'desc': 'String description text',
-     'section': 'example',
-     'key': 'stringexample'},
-    {'type': 'path',
-     'title': 'A path setting',
-     'desc': 'Path description text',
-     'section': 'example',
-     'key': 'pathexample'}])
+     'title': 'File name',
+     'desc': 'Record file name',
+     'section': 'Record',
+     'key': 'File_Name'},
+   # {'type': 'path',
+    # 'title': 'A path setting',
+    # 'desc': 'Path description text',
+   #  'section': 'Record',
+    # 'key': 'pathexample'}
+   ])
+
+
+Upload_settings_json = json.dumps([
+
+     {'type': 'options',
+      'title': 'Access',
+      'desc': 'User level of access',
+      'section': 'Upload',
+      'key': 'AccessSetting',
+      'options': ['reader', 'writer', 'None']},
+
+     {'type': 'string',
+      'title': 'Folder name',
+      'desc': 'The name of the target folder in google disk',
+      'section': 'Upload',
+      'key': 'FileTarget'},
+
+     {'type': 'string',
+      'title': 'User_email',
+      'desc': "Target person's mail",
+      'section': 'Upload',
+      'key': 'User_Email'}])
 
