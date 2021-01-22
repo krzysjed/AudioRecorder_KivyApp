48aa4159c8c54d76470bb96de222c5d8e371850d
Krzysztof Jędrejasz <krzysjed@wp.pl>
2021-01-22 12:34:06 +0000

    Replace settingsjson.py

--- prototype_desktop/settingsjson.py
+++ prototype_desktop/settingsjson.py
@@ -21,6 +21,12 @@ Recording_settings_json = json.dumps([
      'section': 'Record',
      'key': 'SamplingSetting'},
 
+    {'type': 'bool',
+         'title': 'Modifying the playback frequency',
+         'desc': 'OFF - default playback / ON - modify the playback frequency(set your value in the above sampling box',
+         'section': 'Record',
+         'key': 'PlaybackSetting'},
+
     {'type': 'options',
      'title': 'Extension',
      'desc': 'Extension',
@@ -33,9 +39,10 @@ Recording_settings_json = json.dumps([
      'desc': 'Record file name',
      'section': 'Record',
      'key': 'File_Name'},
+
     {'type': 'path',
-     'title': 'A path setting',
-     'desc': 'Path description text',
+     'title': 'A path of audio files',
+     'desc': 'Select the audio file which you want to use',
      'section': 'Record',
      'key': 'PathSetting'}
    ])
